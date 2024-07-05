a3acd369c00a30315d9f0bbf7eee7d9ee796772e
twoBelanson <twobakabaka656@gmail.com>
2024-07-05 15:09:14 -0300

    initial commit

---
+++ bubble.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+//bubble sort
+
+void displayArr(int arr[]);
+
+int main()
+{
+    int numbers[10] = {4, 3, 2, 1, 5, 7, 6, 8, 9, 0};
+    int temp = 0;
+
+    std::cout << "Array original: " << '\n';
+    displayArr(numbers);
+
+    //sort
+    for(int i = 0; i < 10; i++)
+    {
+        for(int j = 0; j < 10; j++)
+        {
+            temp = numbers[j];
+            if(numbers[j] > numbers[j+1])
+            {
+                numbers[j] = numbers[j+1];
+                numbers[j+1] = temp;
+            }
+        }
+    }
+
+    std::cout << "Array sorteado: " << '\n';
+    displayArr(numbers);
+
+    return 0;
+}
+
+void displayArr(int arr[])
+{
+    for(int i = 0; i < 10; i++)
+    {
+        std::cout << arr[i] << ' ';
+    }
+
+    std::cout << '\n';
+}
+
