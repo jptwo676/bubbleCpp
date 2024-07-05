2c47a5928a7dcfcdc0ace7bdc63009dfbf35dbbd
twoBelanson <twobakabaka656@gmail.com>
2024-07-05 15:30:25 -0300

    fixed last number

--- bubble.cpp
+++ bubble.cpp
@@ -8,6 +8,7 @@ int main()
 {
     int numbers[10] = {4, 3, 2, 1, 5, 7, 6, 8, 9, 0};
     int temp = 0;
+    int check = 0;
 
     std::cout << "Array original: " << '\n';
     displayArr(numbers);
@@ -15,7 +16,7 @@ int main()
     //sort
     for(int i = 0; i < 10; i++)
     {
-        for(int j = 0; j < 10; j++)
+        for(int j = 0; j < 9; j++)
         {
             temp = numbers[j];
             if(numbers[j] > numbers[j+1])
@@ -23,7 +24,17 @@ int main()
                 numbers[j] = numbers[j+1];
                 numbers[j+1] = temp;
             }
+            else
+            {
+                check++;
+            }
         }
+        if(check > 4)
+        {
+            std::cout << "Saindo pelo break, i = " << i << '\n';
+            break;
+        }
+        check = 0;
     }
 
     std::cout << "Array sorteado: " << '\n';
